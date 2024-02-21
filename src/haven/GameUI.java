dudesoe-glitch/drekkariclient

commit ee369a8d3349de28dcfffadbf40199cb2f2e1ce5

Added a hack to make menugrid "shortcuts" from belt kinda-sorta work.

src/haven/GameUI.java
@@ -121,7 +121,12 @@ public class GameUI extends ConsoleHost implements Console.Directory {
 	    }
 	    if(local && (menu != null)) {
 		if(res != null) {
-		    MenuGrid.Pagina pag = menu.paginafor(slot.res);
+		    MenuGrid.Pagina pag;
+		    /* XXX: This is a hack. The pagina system needs to be remade. */
+		    if(res != null)
+			pag = menu.paginafor(res.indir());
+		    else
+			pag = menu.paginafor(slot.res);
 		    try {
 			MenuGrid.PagButton btn = pag.button();
 			menu.use(btn, iact, false);

commit 1ef96621d17066e54f05f55679cb3d8b6934da8c

Fix local pagbutton belt-activation bug.
It is strange to imagine that I didn't discover this until now, but it
seems this must have been caused by ee369a8d.

src/haven/GameUI.java
@@ -125,12 +125,10 @@ public class GameUI extends ConsoleHost implements Console.Directory, UI.Message
 	    }
 	    if(local && (menu != null)) {
 		if(res != null) {
-		    MenuGrid.Pagina pag;
-		    /* XXX: This is a hack. The pagina system needs to be remade. */
-		    if(res != null)
+		    MenuGrid.Pagina pag = menu.paginafor(this.res);
+		    /* XXX: This is quite a hack. Is there a better way? */
+		    if(!menu.paginae.contains(pag) && (res != null))
 			pag = menu.paginafor(res.indir());
-		    else
-			pag = menu.paginafor(this.res);
 		    try {
 			MenuGrid.PagButton btn = pag.button();
 			menu.use(btn, iact, false);