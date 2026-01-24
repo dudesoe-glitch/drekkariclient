61f2b89ad4c9bb85fdafa0112aa56d226f5bd3c8
Nightdawg <unqdawg@gmail.com>
2026-01-24 13:08:08 +0200

    Add Narwhal to potential aggro targets (used by attack keybinds)

--- src/haven/automated/AUtils.java
+++ src/haven/automated/AUtils.java
@@ -57,6 +57,7 @@ public class AUtils {
         add("gfx/kritter/woodgrouse/woodgrouse-m");
         add("gfx/kritter/garefowl/garefowl");
         add("gfx/kritter/goshawk/goshawk");
+        add("gfx/kritter/narwhal/narwhal");
 
         add("gfx/kritter/ants/queenant");
         add("gfx/kritter/ants/royalguardant");
