564fd30f09cfc07c88c39f9a55ffee9bd7bd57c2
Nightdawg <unqdawg@gmail.com>
2026-03-05 20:48:56 +0200

    Fix custom client Mine Support Coverage toggle to work with loftar's new support cover toggle

--- src/haven/res/gfx/fx/mscover/Radius.java
+++ src/haven/res/gfx/fx/mscover/Radius.java
@@ -46,7 +46,9 @@ public class Radius extends GAttrib {
 
     public static void parse(Gob gob, Message dat) {
 	try {
-	    gob.setattr(new Radius(gob, (dat.float16() * 11) - ε, true));
+        double rad = (dat.float16() * 11) - ε;
+	    gob.setattr(new Radius(gob, rad, true));
+        gob.msRadSize = (float) rad;
 	} catch(NoClassDefFoundError e) {
 	}
     }
